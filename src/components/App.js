3dbf009abd00a07f53b0525bbbe252be1d851db3
Masha Golovatenko <pikch4y@yandex.com>
2023-05-23 23:19:34 +0300

    feat: create Header, Main and Footer components

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,14 @@
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
 function App() {
   return (
     <body className="root">
       <div className="page">
-        <header className="header ">
-          <a className="header__logo" href="#" target="_blank"></a>
-        </header>
-
-        <main className="container">
-          <section className="profile">
-            <div className="profile__avatar-overlay">
-              <img
-                src="<%=require('./images/profile__avatar.png')%>"
-                alt="Улыбающийся мужчина в красной шапке."
-                className="profile__avatar"
-              />
-            </div>
-            <div className="profile__info">
-              <div className="profile__flex">
-                <h1 className="profile__name">Жак-Ив Кусто</h1>
-                <button
-                  className="profile__edit-button"
-                  type="button"
-                  aria-label="Редактировать"></button>
-              </div>
-              <p className="profile__occupation">Исследователь океана</p>
-            </div>
-            <button className="profile__add-button" type="button" aria-label="Добавить"></button>
-          </section>
-
-          <section className="photos">
-            <template className="card__template">
-              <div className="card">
-                <button className="card__delete-button" aria-label="Нравится"></button>
-                <img className="card__place" />
-                <div className="card__flex">
-                  <h2 className="card__title"></h2>
-                  <div className="card__like-box">
-                    <button className="card__like-button" aria-label="Нравится"></button>
-                    <p className="card__like-counter">0</p>
-                  </div>
-                </div>
-              </div>
-            </template>
-          </section>
-        </main>
-        <footer className="footer">
-          <p className="footer__copyright">&copy; 2023 Mesto Russia</p>
-        </footer>
+        <Header />
+        <Main />
+        <Footer />
 
         <section className="popup popup_edit">
           <div className="popup__container">
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header ">
+      <a className="header__logo" href="#" target="_blank"></a>
+    </header>
+  );
+}
+
+export default Header;
